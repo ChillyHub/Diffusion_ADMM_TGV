61b972de087e088901c59ca3c50d7b5bd7c73653
Ben Chilly <1533935729@qq.com>
2024-06-05 15:28:08 +0800

    commit

--- experience.py
+++ experience.py
@@ -6,15 +6,16 @@ import generator_AAPM_3d_limitedangle
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Experience.')
 
-    parser.add_argument('--view', type=int, default=0, help='Number of views')
+    parser.add_argument('--view', type=str, default='8,4,2,90', help='Number of views')
     parser.add_argument('--method', type=str, default='', help='Method')
     parser.add_argument('--device', type=str, default='', help='Device')
     parser.add_argument('--batch_size', type=int, default=12, help='Batch size')
     args = parser.parse_args()
 
+    views = args.view.split(',')
     methods = args.method.split(',')
 
-    if args.view == 8 or args.view == 0:
+    if '8' in views:
         if 'TGV_3' in methods or 'TGV' in methods or args.method == '':
             generator_AAPM_3d_sparseview.run(p_metho='TGV_3', p_n_view=8, p_rho_0=5, p_rho_1=5, p_alpha_0=2.5, p_alpha_1=2.5, p_lam=0.04, device=args.device, batch_size=args.batch_size)
         if 'TGV_2' in methods or 'TGV' in methods or args.method == '':
@@ -24,7 +25,7 @@ if __name__ == '__main__':
         if 'TV' in methods or args.method == '':
             generator_AAPM_3d_sparseview.run(p_metho='TV', p_n_view=8, p_rho_0=10, p_lam=0.04, device=args.device, batch_size=args.batch_size)
 
-    if args.view == 4 or args.view == 0:
+    if '4' in views:
         if 'TGV_3' in methods or 'TGV' in methods or args.method == '':
             generator_AAPM_3d_sparseview.run(p_metho='TGV_3', p_n_view=4, p_rho_0=5, p_rho_1=5, p_alpha_0=2.5, p_alpha_1=2.5, p_lam=0.04, device=args.device, batch_size=args.batch_size)
         if 'TGV_2' in methods or 'TGV' in methods or args.method == '':
@@ -34,7 +35,7 @@ if __name__ == '__main__':
         if 'TV' in methods or args.method == '':
             generator_AAPM_3d_sparseview.run(p_metho='TV', p_n_view=4, p_rho_0=10, p_lam=0.04, device=args.device, batch_size=args.batch_size)
 
-    if args.view == 2 or args.view == 0:
+    if '2' in views:
         if 'TGV_3' in methods or 'TGV' in methods or args.method == '':
             generator_AAPM_3d_sparseview.run(p_metho='TGV_3', p_n_view=2, p_rho_0=5, p_rho_1=5, p_alpha_0=2.5, p_alpha_1=2.5, p_lam=0.04, device=args.device, batch_size=args.batch_size)
         if 'TGV_2' in methods or 'TGV' in methods or args.method == '':
@@ -44,7 +45,7 @@ if __name__ == '__main__':
         if 'TV' in methods or args.method == '':
             generator_AAPM_3d_sparseview.run(p_metho='TV', p_n_view=2, p_rho_0=10, p_lam=0.04, device=args.device, batch_size=args.batch_size)
 
-    if args.view == 90 or args.view == 0:
+    if '90' in views:
         if 'TGV_3' in methods or 'TGV' in methods or args.method == '':
             generator_AAPM_3d_limitedangle.run(p_metho='TGV_3', p_n_view=90, p_rho_0=5, p_rho_1=5, p_alpha_0=2.5, p_alpha_1=2.5, p_lam=0.04, device=args.device, batch_size=args.batch_size)
         if 'TGV_2' in methods or 'TGV' in methods or args.method == '':
